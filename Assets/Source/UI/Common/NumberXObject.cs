fd07754b227884e1c769c97a0e97489e3431dc4a
hayannal2009 <hayannal2009@gmail.com>
2022-09-08 05:12:28 +0900

    NumberX Object 프리팹 추가.

---
+++ Assets/Source/UI/Common/NumberXObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberXObject : MonoBehaviour
+{
+	public GameObject xObject;
+	public GameObject[] numberObjectList;
+
+	public void SetNumber(int number)
+	{
+		xObject.SetActive(number >= 1);
+
+		for (int i = 0; i < numberObjectList.Length; ++i)
+			numberObjectList[i].SetActive(i == number);
+	}
+
+	Transform _transform;
+	public Transform cachedTransform
+	{
+		get
+		{
+			if (_transform == null)
+				_transform = GetComponent<Transform>();
+			return _transform;
+		}
+	}
+}
