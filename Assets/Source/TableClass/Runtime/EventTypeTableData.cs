06ad6dc24e2dee9928f1dd3bafc23ad3fc747c75
hayannal2009 <hayannal2009@gmail.com>
2022-09-10 03:24:40 +0900

    이벤트타입 테이블에 컬럼 추가.

--- Assets/Source/TableClass/Runtime/EventTypeTableData.cs
+++ Assets/Source/TableClass/Runtime/EventTypeTableData.cs
@@ -13,7 +13,43 @@ public class EventTypeTableData
   public string id { get { return _id; } set { _id = value; } }
   
   [SerializeField]
+  string _referEvent;
+  public string referEvent { get { return _referEvent; } set { _referEvent = value; } }
+  
+  [SerializeField]
   int _givenTime;
   public int givenTime { get { return _givenTime; } set { _givenTime = value; } }
+  
+  [SerializeField]
+  int _coolTime;
+  public int coolTime { get { return _coolTime; } set { _coolTime = value; } }
+  
+  [SerializeField]
+  int _startYear;
+  public int startYear { get { return _startYear; } set { _startYear = value; } }
+  
+  [SerializeField]
+  int _startMonth;
+  public int startMonth { get { return _startMonth; } set { _startMonth = value; } }
+  
+  [SerializeField]
+  int _startDay;
+  public int startDay { get { return _startDay; } set { _startDay = value; } }
+  
+  [SerializeField]
+  int _endYear;
+  public int endYear { get { return _endYear; } set { _endYear = value; } }
+  
+  [SerializeField]
+  int _endMonth;
+  public int endMonth { get { return _endMonth; } set { _endMonth = value; } }
+  
+  [SerializeField]
+  int _endDay;
+  public int endDay { get { return _endDay; } set { _endDay = value; } }
+  
+  [SerializeField]
+  int _triggerCondition;
+  public int triggerCondition { get { return _triggerCondition; } set { _triggerCondition = value; } }
   
 }
