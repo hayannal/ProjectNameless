3266048ca9af4a18facae452c623ccb58d6c1a3c
hayannal2009 <hayannal2009@gmail.com>
2022-10-24 15:30:11 +0900

    프리뷰 오브젝트 각도 조절 위치 조절 빌보드 적용

---
+++ Assets/Source/Map/PreviewObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewObject : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+		cachedTransform.eulerAngles = CustomFollowCamera.instance.cachedTransform.eulerAngles;
+    }
+
+
+
+	Transform _transform;
+	public Transform cachedTransform
+	{
+		get
+		{
+			if (_transform == null)
+				_transform = GetComponent<Transform>();
+			return _transform;
+		}
+	}
+}
