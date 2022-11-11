7a00a57839a608e4c4bc132104bf3a6f75a0902a
hayannal2009 <hayannal2009@gmail.com>
2022-11-12 01:20:55 +0900

    미션리스트캔버스 작업중

---
+++ Assets/Source/UI/MainMenu/MissionListCanvas.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionListCanvas : MonoBehaviour
+{
+	void OnEnable()
+	{
+		bool restore = StackCanvas.Push(gameObject, false, null, OnPopStack);
+
+		if (DragThresholdController.instance != null)
+			DragThresholdController.instance.ApplyUIDragThreshold();
+
+		if (restore)
+			return;
+
+		MainCanvas.instance.OnEnterCharacterMenu(true);
+	}
+
+	void OnDisable()
+	{
+		if (DragThresholdController.instance != null)
+			DragThresholdController.instance.ResetUIDragThreshold();
+
+		if (StackCanvas.Pop(gameObject))
+			return;
+
+		OnPopStack();
+	}
+
+	void OnPopStack()
+	{
+		if (StageManager.instance == null)
+			return;
+		if (MainSceneBuilder.instance == null)
+			return;
+
+		MainCanvas.instance.OnEnterCharacterMenu(false);
+	}
+
+	public void OnClickButton(int index)
+	{
+		switch (index)
+		{
+			case 0:
+				UIInstanceManager.instance.ShowCanvasAsync("PetSearchCanvas", null);
+				break;
+		}
+	}
+}
