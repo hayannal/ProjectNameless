be9a122ce5e8f3d65ffec68c824e665310c33432
hayannal2009 <hayannal2009@gmail.com>
2022-11-16 04:06:35 +0900

    팀포지션 셀렉트 캔버스 추가.

---
+++ Assets/Source/UI/MainMenu/Character/TeamPositionCanvas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamPositionCanvas : MonoBehaviour
+{
+	public CharacterCanvasListItem leftCharacterCanvasListItem;
+	public CharacterCanvasListItem rightCharacterCanvasListItem;
+
+	void OnEnable()
+	{
+		RefreshSlot();
+	}
+
+	void RefreshSlot()
+	{
+		if (string.IsNullOrEmpty(CharacterManager.instance.leftCharacterId) == false)
+		{
+			CharacterData characterData = CharacterManager.instance.GetCharacterData(CharacterManager.instance.leftCharacterId);
+			if (characterData != null)
+			{
+				leftCharacterCanvasListItem.Initialize(characterData.actorId, characterData.level, characterData.transcend, 0, null, null, null);
+				leftCharacterCanvasListItem.equippedObject.SetActive(false);
+				leftCharacterCanvasListItem.gameObject.SetActive(true);
+			}
+		}
+
+		if (string.IsNullOrEmpty(CharacterManager.instance.rightCharacterId) == false)
+		{
+			CharacterData characterData = CharacterManager.instance.GetCharacterData(CharacterManager.instance.rightCharacterId);
+			if (characterData != null)
+			{
+				rightCharacterCanvasListItem.Initialize(characterData.actorId, characterData.level, characterData.transcend, 0, null, null, null);
+				rightCharacterCanvasListItem.equippedObject.SetActive(false);
+				rightCharacterCanvasListItem.gameObject.SetActive(true);
+			}
+		}
+	}
+
+	public void OnClickLeftButton()
+	{
+
+	}
+
+	public void OnClickRightButton()
+	{
+
+	}
+}
