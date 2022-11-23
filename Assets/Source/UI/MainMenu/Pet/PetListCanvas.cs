1017cb68a2b10277aad9d5f6572637dffd756c8b
hayannal2009 <hayannal2009@gmail.com>
2022-11-24 04:49:58 +0900

    _noGain 잘못 쓰던거 수정

--- Assets/Source/UI/MainMenu/Pet/PetListCanvas.cs
+++ Assets/Source/UI/MainMenu/Pet/PetListCanvas.cs
@@ -167,7 +167,7 @@ public class PetListCanvas : PetShowCanvasBase
 
 		for (int i = 0; i < _listTempTableData.Count; ++i)
 		{
-			PetCanvasListItem petCanvasListItem = _container.GetCachedItem(contentItemPrefab, noGainContentRootRectTransform);
+			PetCanvasListItem petCanvasListItem = _noGainContainer.GetCachedItem(contentItemPrefab, noGainContentRootRectTransform);
 			petCanvasListItem.Initialize(_listTempTableData[i], 0, _listTempTableData[i].accumulatedAtk, OnClickListItem);
 			_listPetCanvasListItem.Add(petCanvasListItem);
 		}
