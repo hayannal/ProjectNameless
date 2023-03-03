a92cebd8d50730e0df998d10dace62b4a80b1042
hayannal2009 <hayannal2009@gmail.com>
2023-03-03 23:04:00 +0900

    첫구매 보상 로직 적용. 스펠 보상 표시.

--- Assets/Source/UI/CashShop/FirstPurchaseCanvas.cs
+++ Assets/Source/UI/CashShop/FirstPurchaseCanvas.cs
@@ -5,22 +5,49 @@ using MEC;
 
 public class FirstPurchaseCanvas : MonoBehaviour
 {
+	public SkillIcon skillIcon;
+	public RewardIcon[] rewardIconList;
+
 	public GameObject shopButtonObject;
 	public GameObject rewardButtonObject;
 
 	public RectTransform alarmRootTransform;
 
+	void Start()
+	{
+		// 그냥 두기로 한다.
+		//for (int i = 0; i < rewardIconList.Length; ++i)
+		//	rewardIconList[i].ShowOnlyIcon(false, 1.0f);
+	}
+
+	SkillTableData _skillTableData;
 	ShopProductTableData _shopProductTableData;
 	void OnEnable()
 	{
+		_shopProductTableData = TableDataManager.instance.FindShopProductTableData("firstpurchase");
+
+		// unacquiredSpellSelectedId
+		string selectedSpellId = _shopProductTableData.rewardValue1;
+		if (string.IsNullOrEmpty(selectedSpellId))
+			return;
+		SkillTableData skillTableData = TableDataManager.instance.FindSkillTableData(selectedSpellId);
+		if (skillTableData == null)
+			return;
+		skillIcon.SetInfo(skillTableData, false);
+		_skillTableData = skillTableData;
+
 		shopButtonObject.SetActive(PlayerData.instance.vtd == 0);
 		rewardButtonObject.SetActive(PlayerData.instance.vtd > 0);
-
-		_shopProductTableData = TableDataManager.instance.FindShopProductTableData("firstpurchase");
 
 		AlarmObject.Hide(alarmRootTransform);
 		if (PlayerData.instance.vtd > 0)
 			AlarmObject.Show(alarmRootTransform);
+	}
+
+	public void OnClickDetailButton()
+	{
+		// 첫번째 항목이 스펠 고정이다.
+		RewardIcon.ShowDetailInfo(_shopProductTableData.rewardType1, _shopProductTableData.rewardValue1);
 	}
 
 	public void OnClickShopButton()
@@ -56,16 +83,29 @@ public class FirstPurchaseCanvas : MonoBehaviour
 
 			if (shopProductTableData != null)
 			{
-				CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType1, shopProductTableData.rewardValue1, shopProductTableData.rewardCount1);
+				float prevPowerValue = BattleInstanceManager.instance.playerActor.actorStatus.GetValue(ActorStatusDefine.eActorStatus.CombatPower);
+
+				// 인앱결제는 아니지만 보상이 스펠 고정이기때문에 캐시상품 구매하는거처럼 처리한다.
+				//CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType1, shopProductTableData.rewardValue1, shopProductTableData.rewardCount1);
+				if (shopProductTableData.rewardType1 == "it")
+					SpellManager.instance.OnRecvPurchaseItem(shopProductTableData.rewardValue1, shopProductTableData.rewardCount1);
+
+				float nextValue = BattleInstanceManager.instance.playerActor.actorStatus.GetValue(ActorStatusDefine.eActorStatus.CombatPower);
+				if (nextValue > prevPowerValue)
+				{
+					// 변경 완료를 알리고
+					UIInstanceManager.instance.ShowCanvasAsync("ChangePowerCanvas", () =>
+					{
+						ChangePowerCanvas.instance.ShowInfo(prevPowerValue, nextValue);
+					});
+				}
+
 				CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType2, shopProductTableData.rewardValue2, shopProductTableData.rewardCount2);
 				CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType3, shopProductTableData.rewardValue3, shopProductTableData.rewardCount3);
-				CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType4, shopProductTableData.rewardValue4, shopProductTableData.rewardCount4);
-				CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType5, shopProductTableData.rewardValue5, shopProductTableData.rewardCount5);
+				//CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType4, shopProductTableData.rewardValue4, shopProductTableData.rewardCount4);
+				//CurrencyData.instance.OnRecvProductReward(shopProductTableData.rewardType5, shopProductTableData.rewardValue5, shopProductTableData.rewardCount5);
 
-				UIInstanceManager.instance.ShowCanvasAsync("CommonRewardCanvas", () =>
-				{
-					CommonRewardCanvas.instance.RefreshReward(shopProductTableData);
-				});
+				ToastCanvas.instance.ShowToast(UIString.instance.GetString("ShopUI_GotFreeItem"), 2.0f);
 			}
 
 			gameObject.SetActive(false);
